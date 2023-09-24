b22b3f98eaa532743666ed2677771765ce6eabad
rhobread <101875755+rhobread@users.noreply.github.com>
2023-09-24 14:02:41 +0700

    Delete Bad Code Smell Encapsulation directory

--- Bad Code Smell Encapsulation/Missing Encapsulation/After/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        
-        Employee employee1 = new Employee("John","Male",26,10000,60);
-        Display.displayEmployees(employee1);
-    }
-}
